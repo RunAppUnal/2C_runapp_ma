0627c55a93ef0637191d644ac8381097c8acfc73
Jesus Felipe Chavarro Muñoz <jfchavarrom@unal.edu.co>
2018-04-09 17:46:22 -0500

    main activity layout and login fix

--- runapp-ma/app/src/main/java/com/runapp/runapp_ma/MainActivity.java
+++ runapp-ma/app/src/main/java/com/runapp/runapp_ma/MainActivity.java
@@ -1,29 +1,30 @@
 package com.runapp.runapp_ma;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private SharedPreferences sharedPrefes = getSharedPreferences("userData", MODE_PRIVATE);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        if (sharedPrefes.getBoolean("logged", false)) {
+            Intent myIntent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(myIntent);
+        }else{
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        }
+
+
     }
 
+    public void toBikeRoutesActivity(View view) {
+    }
+
+    public void toCarPoolActivity(View view) {
+    }
 }
